8a209dbaf16c95c2364997dd64ec0f451e9b8e4e
manojbandari <bandarimanoj22@msitprogram.net>
2018-12-18 17:23:57 +0530

    Assignment2-udp

---
+++ module 8/udpClient.py
@@ -0,0 +1,23 @@
+import socket
+
+
+def main():
+	host = '127.0.0.1'
+	port = 5001
+	
+
+	server=('127.0.0.1',5000)
+
+	s= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind((host,port))
+
+	message = input("Enter message:  ")
+	while message !='q':
+		s.sendto(str(message).encode('ASCII'),server)
+		data, addr = s.recvfrom(1024)
+		print("Recieved from server: "+ data.decode('ASCII'))
+		message = input("Enter another message: ")
+	s.close()
+
+if __name__=='__main__':
+ main()
